9caf9ade8ff87a1cb94fb612df4abd96fb5ed239
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2013-11-01 00:10:09 +0100

    Create shared ffmpeg

--- contrib/build-scripts/build-ffmpeg-linux.sh
+++ contrib/build-scripts/build-ffmpeg-linux.sh
@@ -30,7 +30,7 @@ export PATH=${PATH}:${DEST_DIR}/bin
 
 git clone --depth 1 git://git.videolan.org/x264.git
 cd x264
-./configure --prefix="${DEST_DIR}" --bindir="${DEST_DIR}/bin" --enable-static --enable-pic
+./configure --prefix="${DEST_DIR}" --bindir="${DEST_DIR}/bin" --enable-shared --enable-pic
 make
 make install
 cd ..
@@ -38,7 +38,7 @@ cd ..
 git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
 cd fdk-aac
 autoreconf -fiv
-./configure --prefix="${DEST_DIR}" --disable-shared --with-pic
+./configure --prefix="${DEST_DIR}" --enable-shared --with-pic
 make
 make install
 cd ..
@@ -46,14 +46,14 @@ cd ..
 wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
 tar xzvf opus-1.0.3.tar.gz
 cd opus-1.0.3
-./configure --prefix="${DEST_DIR}" --disable-shared --with-pic
+./configure --prefix="${DEST_DIR}" --enable-shared --with-pic
 make
 make install
 cd ..
 
 git clone --depth 1 http://git.chromium.org/webm/libvpx.git
 cd libvpx
-./configure --prefix="${DEST_DIR}" --disable-examples --enable-pic
+./configure --prefix="${DEST_DIR}" --disable-examples --enable-shared --enable-pic
 make
 make install
 cd ..
@@ -63,8 +63,9 @@ cd ffmpeg
 PKG_CONFIG_PATH="${DEST_DIR}/lib/pkgconfig"
 export PKG_CONFIG_PATH
 ./configure --prefix="${DEST_DIR}" \
-  --extra-cflags="-I${DEST_DIR}/include" --extra-ldflags="-L${DEST_DIR}/lib" --prefix="${DEST_DIR}" \
-  --bindir="${DEST_DIR}/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree --enable-pic
+  --extra-cflags="-I${DEST_DIR}/include" --extra-ldflags="-L${DEST_DIR}/lib" \
+  --bindir="${DEST_DIR}/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree \
+  --enable-pic --disable-programs --disable-doc --enable-shared
 make
 make install
 cd ..
