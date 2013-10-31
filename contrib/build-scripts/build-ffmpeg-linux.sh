9254de87a25a4d91a30886ce361736fcfb642acb
Stefan Radomski <radomski@tk.informatik.tu-darmstadt.de>
2013-10-31 22:43:07 +0100

    Enable PIC with ffmpeg builds

--- contrib/build-scripts/build-ffmpeg-linux.sh
+++ contrib/build-scripts/build-ffmpeg-linux.sh
@@ -30,7 +30,7 @@ export PATH=${PATH}:${DEST_DIR}/bin
 
 git clone --depth 1 git://git.videolan.org/x264.git
 cd x264
-./configure --prefix="${DEST_DIR}" --bindir="${DEST_DIR}/bin" --enable-static
+./configure --prefix="${DEST_DIR}" --bindir="${DEST_DIR}/bin" --enable-static --enable-pic
 make
 make install
 cd ..
@@ -38,7 +38,7 @@ cd ..
 git clone --depth 1 git://git.code.sf.net/p/opencore-amr/fdk-aac
 cd fdk-aac
 autoreconf -fiv
-./configure --prefix="${DEST_DIR}" --disable-shared
+./configure --prefix="${DEST_DIR}" --disable-shared --with-pic
 make
 make install
 cd ..
@@ -46,14 +46,14 @@ cd ..
 wget http://downloads.xiph.org/releases/opus/opus-1.0.3.tar.gz
 tar xzvf opus-1.0.3.tar.gz
 cd opus-1.0.3
-./configure --prefix="${DEST_DIR}" --disable-shared
+./configure --prefix="${DEST_DIR}" --disable-shared --with-pic
 make
 make install
 cd ..
 
 git clone --depth 1 http://git.chromium.org/webm/libvpx.git
 cd libvpx
-./configure --prefix="${DEST_DIR}" --disable-examples
+./configure --prefix="${DEST_DIR}" --disable-examples --enable-pic
 make
 make install
 cd ..
@@ -64,7 +64,7 @@ PKG_CONFIG_PATH="${DEST_DIR}/lib/pkgconfig"
 export PKG_CONFIG_PATH
 ./configure --prefix="${DEST_DIR}" \
   --extra-cflags="-I${DEST_DIR}/include" --extra-ldflags="-L${DEST_DIR}/lib" --prefix="${DEST_DIR}" \
-  --bindir="${DEST_DIR}/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree
+  --bindir="${DEST_DIR}/bin" --extra-libs="-ldl" --enable-gpl --enable-nonfree --enable-pic
 make
 make install
 cd ..
