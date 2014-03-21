0cea71f81934497ae62e0ea53ed9ed5d2b5f7779
enricop <enricop@computer.org>
2014-03-21 16:37:25 +0100

    changed parsing test

--- test/src/test-arabica-parsing.cpp
+++ test/src/test-arabica-parsing.cpp
@@ -14,43 +14,39 @@ using namespace Arabica::DOM;
 
 int main(int argc, char** argv) {
 
+    Arabica::SAX2DOM::Parser<std::string> domParser;
+    Arabica::SAX::CatchErrorHandler<std::string> errorHandler;
+    domParser.setErrorHandler(errorHandler);
+
     int age2;
     std::cin >> age2;
 
-    for (int j=0; j<100; j++) {
-	{
-		std::stringstream* ss = new std::stringstream();
-        (*ss) << "<root>\n<![CDATA[\n< \" ' < > &\n]]>\n</root>";
-		// we need an auto_ptr for arabica to assume ownership
-		std::auto_ptr<std::istream> ssPtr(ss);
-		Arabica::SAX::InputSource<std::string> inputSource(ssPtr);
+        for (int j=0; j<100; j++) {
+        {
+            std::stringstream* ss = new std::stringstream();
+            (*ss) << "<root>\n<![CDATA[\n< \" ' < > &\n]]>\n</root>";
+            // we need an auto_ptr for arabica to assume ownership
+            std::auto_ptr<std::istream> ssPtr(ss);
+            Arabica::SAX::InputSource<std::string> inputSource(ssPtr);
 
-		Arabica::SAX2DOM::Parser<std::string> domParser;
-		Arabica::SAX::CatchErrorHandler<std::string> errorHandler;
-		domParser.setErrorHandler(errorHandler);
+            if(!domParser.parse(inputSource)) {
+                std::cout << errorHandler.errors();
+                return -1;
+            }
+            std::cout << domParser.getDocument().getDocumentElement().getFirstChild().getNodeValue() << std::endl;
+            std::cout << domParser.getDocument() << std::endl;
+        }
+        {
+            Arabica::SAX::InputSource<std::string> inputSource;
+            inputSource.setSystemId("/home/sunkiss/_Projects/uscxml/autoware_tim_test.scxml.xml");
 
-		if(!domParser.parse(inputSource)) {
-			std::cout << errorHandler.errors();
-			return -1;
-		}
-		std::cout << domParser.getDocument().getDocumentElement().getFirstChild().getNodeValue() << std::endl;
-		std::cout << domParser.getDocument() << std::endl;
-	}
-	{
-		Arabica::SAX::InputSource<std::string> inputSource;
-        inputSource.setSystemId("/home/sunkiss/_Projects/uscxml/autoware_tim_test.scxml.xml");
-
-		Arabica::SAX2DOM::Parser<std::string> domParser;
-		Arabica::SAX::CatchErrorHandler<std::string> errorHandler;
-		domParser.setErrorHandler(errorHandler);
-
-		if(!domParser.parse(inputSource)) {
-			std::cout << errorHandler.errors();
-			return -1;
-		}
-		std::cout << domParser.getDocument() << std::endl;
-    }
-    }
+            if(!domParser.parse(inputSource)) {
+                std::cout << errorHandler.errors();
+                return -1;
+            }
+            std::cout << domParser.getDocument() << std::endl;
+        }
+        }
 
     int age;
     std::cin >> age;
