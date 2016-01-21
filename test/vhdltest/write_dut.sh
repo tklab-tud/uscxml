a2434fc3a89b9cdc0d39bdcfdd6f0864388c269e
juehv <jens@jensheuschkel.de>
2016-01-21 14:30:02 +0100

    improve test scripts

--- test/vhdltest/write_dut.sh
+++ test/vhdltest/write_dut.sh
@@ -5,8 +5,8 @@ SIM_DIR=/home/juehv/modelsim/automated_test/
 INSTALL_DIR=/home/juehv/altera/13.1/modelsim_ase/bin/
 VHDL_OUT=${SIM_DIR}vhd/
 
-COMPILE_CMD="${INSTALL_DIR}vcom ${VHDL_OUT}dut.vhd ${VHDL_OUT}testbench.vhd"
-SIMULATION_CMD="${INSTALL_DIR}vsim -c scxml_lib.testbench -do automation.tcl"
+COMPILE_CMD="${INSTALL_DIR}vcom ${VHDL_OUT}dut.vhd "
+SIMULATION_CMD="${INSTALL_DIR}vsim -c scxml_lib.dut -do automation.tcl"
 
 # get arguments
 SCXML_TEST="/home/juehv/git/uscxml/test/w3c/ecma/test144.scxml"
@@ -23,6 +23,7 @@ echo "${VHDL_OUT}dut.vhd written"
 TMP_RESULT="$(tail -n 1 ${VHDL_OUT}dut.vhd)"
 
 if [ "$TMP_RESULT" == "ERROR" ] ; then
+  echo "$(cat ${VHDL_OUT}dut.vhd)"
   echo "Error while generating VHDL"
   echo "FAILED"
   exit -1
@@ -32,7 +33,6 @@ fi
 # compile stuff
 cd ${SIM_DIR}
 ${COMPILE_CMD}
-#/home/juehv/altera/13.1/modelsim_ase/bin/vcom  ${VHDL_OUT}dut.vhd testbench.vhd
 
 if [ $? -eq 0 ] ; then
     echo "compilation done."
@@ -41,7 +41,21 @@ else
     echo "FAILED"
     exit -1
 fi
-
+exit 0
 # simulate the hardware
 # simulator will drop FAILED or PASSED
-${SIMULATION_CMD}
+cd ${SIM_DIR}
+#${SIMULATION_CMD}
+
+
+TMP_RESULT="$(tail -n 1 ${SIM_DIR}transcript)"
+
+if [ "$TMP_RESULT" != "OK" ] ; then
+  echo "Error while simulating VHDL"
+  echo "$TMP_RESULT"
+  exit -1
+else 
+  echo "OK"
+  exit 0
+fi
+
