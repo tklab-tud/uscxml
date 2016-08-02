41a65bd4e3d946c42825813836e58ba1030dcdd9
Stefan Radomski <github@mintwerk.de>
2016-08-02 22:52:26 +0200

    Fixed 64bit MSVC builds

--- contrib/cmake/BuildURIParser.cmake
+++ contrib/cmake/BuildURIParser.cmake
@@ -25,15 +25,18 @@ if (UNIX)
 elseif(MSVC)
 		if( CMAKE_SIZEOF_VOID_P EQUAL 8 )
 			# we cannot build 64bit with the given project -> patch
+			set(VSPROJECT_PATH "win32/uriparser-vs2013")
+
 			externalproject_add(uriparser
 				URL http://ufpr.dl.sourceforge.net/project/uriparser/Sources/0.8.4/uriparser-0.8.4.zip
 				URL_MD5 a24c9ba0fd1a2c6c75d02859bf337cfa
 				BUILD_IN_SOURCE 1
 				PREFIX ${CMAKE_BINARY_DIR}/deps/uriparser
 				PATCH_COMMAND
-					${CMAKE_COMMAND} -E copy_directory ${PROJECT_SOURCE_DIR}/contrib/patches/uriparser/uriparser.vcxproj <SOURCE_DIR>/win32/uriparser-vs2013
+					${CMAKE_COMMAND} -E make_directory <SOURCE_DIR>/win32/uriparser-vs2013/ &&
+					${CMAKE_COMMAND} -E copy ${PROJECT_SOURCE_DIR}/contrib/patches/uriparser/uriparser.vcxproj <SOURCE_DIR>/win32/uriparser-vs2013/
 				CONFIGURE_COMMAND ""
-				BUILD_COMMAND cd win32/uriparser-vs2013 && devenv /Upgrade uriparser.vcxproj && msbuild /p:Configuration=Release /p:Platform=x64 /t:build uriparser.vcxproj
+				BUILD_COMMAND cd ${VSPROJECT_PATH} && devenv /Upgrade uriparser.vcxproj && msbuild /p:Configuration=Release /p:Platform=Win32 /t:build uriparser.vcxproj
 				INSTALL_COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_BINARY_DIR}/deps/uriparser/lib && ${CMAKE_COMMAND} -E copy win32/uriparser.lib ${CMAKE_BINARY_DIR}/deps/uriparser/lib/ && ${CMAKE_COMMAND} -E copy_directory include ${CMAKE_BINARY_DIR}/deps/uriparser/include
 			)
 		else()
