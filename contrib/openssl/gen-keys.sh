1e84bbb24e8301ccfcf8ffd98f00447fa566c8a6
Stefan Radomski <github@mintwerk.de>
2017-05-30 22:46:05 +0200

    Be more explicit with key names in contrib

--- contrib/openssl/gen-keys.sh
+++ contrib/openssl/gen-keys.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-openssl req -x509 -sha256 -nodes -newkey rsa:4096 -keyout uscxml.key -out uscxml.cert
+#
+# HTTPS support via keypairs:
+# uscxml-browser --private-key=uscxml.key --public-key=uscxml.pub
+#
+
+openssl req -x509 -sha256 -nodes -newkey rsa:4096 -keyout uscxml.key -out uscxml.pub
